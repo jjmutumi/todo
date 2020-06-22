5eb5215fcd187ebd421ca8c0f7f6fc374305098d
Joseph Mutumi <jjmutumi@gmail.com>
2020-06-22 19:19:09 +0300

    TodoRepository:
    - Adding flag to know if it's loading.

--- lib/services/todo_repository.dart
+++ lib/services/todo_repository.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:todo/models/todo.dart';
 
 abstract class TodoRepository extends ChangeNotifier {
+  bool isLoading = false;
   List<Todo> todos = List<Todo>();
 
   Future save(Todo todo);
