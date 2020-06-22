683da8c2a275f98f9b5575a7504904d32ed1e40f
Joseph Mutumi <jjmutumi@gmail.com>
2020-06-22 13:54:05 +0300

    Adding user model.

---
+++ lib/models/user.dart
@@ -0,0 +1,16 @@
+class User {
+  User({this.name});
+
+  String name;
+
+  /// Similar to Java:
+  /// public static User fromJson(Map<String, dynamic> json);
+  /// 
+  /// User user = User.fromJson(map);
+  /// 
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        name: json["name"] ?? null,
+      );
+
+  Map<String, dynamic> toJson() => {"name": name};
+}
