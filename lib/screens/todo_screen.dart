67ae7d10edd5d66d306d606b71c02494fa6524f7
Joseph Mutumi <jjmutumi@gmail.com>
2020-06-22 20:49:51 +0300

    Using Provider (1):
    - Injecting wired implementation of TodoRepository
    instead of making a concrete instance in code.
    - No need to call setState because Consumer rebuilds when TodoRepository
    state changes

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo/components/todo_screen_app_bar.dart';
 import 'package:todo/components/todo_screen_form.dart';
 import 'package:todo/components/todo_screen_todo_list.dart';
-import 'package:todo/services/shared_preferences_todo_repository.dart';
 import 'package:todo/services/todo_repository.dart';
 
 class TodoScreen extends StatefulWidget {
@@ -11,29 +11,26 @@ class TodoScreen extends StatefulWidget {
 }
 
 class _TodoScreenState extends State<TodoScreen> {
-  TodoRepository todoRepository;
-
   @override
   void initState() {
     super.initState();
-    todoRepository = SharedPreferencesTodoRepository();
-    todoRepository.addListener(() {
-      setState(() {});
-    });
-    todoRepository.fetch();
+    Provider.of<TodoRepository>(context, listen: false).fetch();
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: CustomScrollView(
-          slivers: <Widget>[
-            TodoScreenAppBar(),
-            TodoScreenForm(todoRepository: todoRepository),
-            TodoScreenTodoList(todoRepository: todoRepository),
-          ],
-        ),
+        body:
+            Consumer<TodoRepository>(builder: (context, todoRepository, child) {
+          return CustomScrollView(
+            slivers: <Widget>[
+              TodoScreenAppBar(),
+              TodoScreenForm(todoRepository: todoRepository),
+              TodoScreenTodoList(todoRepository: todoRepository),
+            ],
+          );
+        }),
       ),
     );
   }
